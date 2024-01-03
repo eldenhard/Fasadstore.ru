a58e7ccd74c382e94968dbb99f3361ef2ae482a5
eldenhard <elden.hard@mail.ru>
2024-01-03 18:02:51 +0300

    навправления деятельности

--- src/components/DirectionJob/DirectionWork.vue
+++ src/components/DirectionJob/DirectionWork.vue
@@ -1,13 +1,192 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-block">
     <div class="direction_description">
-      <p>Наши направления деятельности</p>
+      <p>НАПРАВЛЕНИЯ ДЕЯТЕЛЬНОСТИ</p>
     </div>
-    <section class="direction_work"></section>
+    <ul class="cards">
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Окна ALUTECH</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Алюминевые раздвижные двери</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Остекление помещений</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Остекление фасадов</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Остекление фасадов</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="card">
+          <img src="./assets/direc1.jpg" class="card__image" alt="" />
+          <div class="card__overlay">
+            <div class="card__header">
+              <div class="card__header-text">
+                <h3 class="card__title">Остекление фасадов</h3>
+                <span class="card__status">Алютех</span>
+              </div>
+            </div>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?
+            </p>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 2rem;
+  margin: 4rem 5vw;
+  padding: 0;
+  list-style-type: none;
+}
+
+.card {
+  position: relative;
+  display: block;
+  height: 100%;
+  border-radius: calc(8 * 1px);
+  overflow: hidden;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.card__image {
+  width: 100%;
+  height: auto;
+}
+
+.card__overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  border-radius: calc(8 * 1px);
+  background-color: white;
+  transform: translateY(100%);
+  transition: 0.2s ease-in-out;
+}
+
+.card:hover .card__overlay {
+  transform: translateY(0);
+}
+
+.card__header {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 2em;
+  padding: 2em;
+  border-radius: calc(20 * 1px) 0 0 0;
+  background-color: white;
+  transform: translateY(-100%);
+  transition: 0.2s ease-in-out;
+}
+
+.card:hover .card__header {
+  transform: translateY(0);
+}
+
+.card__title {
+  font-size: 1em;
+  margin: 0 0 0.3em;
+  color: #6a515e;
+}
+
+.card__status {
+  font-size: 0.8em;
+  color: #d7bdca;
+}
+
+.card__description {
+  padding: 0 2em 2em;
+  margin: 0;
+  color: black;
+  font-family: 'MockFlowFont';
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+
+.container-block {
+  width: 75%;
+  margin: 0 auto;
+}
 .direction_description {
   color: white;
   margin-top: 4%;
@@ -16,5 +195,13 @@
   font-weight: 700;
   letter-spacing: 0;
   line-height: 1em;
+}
+
+@media screen and (max-width: 900px) {
+  .direction_description {
+    text-align: center;
+    white-space: nowrap;
+    margin-top: 4%;
+  }
 }
 </style>
