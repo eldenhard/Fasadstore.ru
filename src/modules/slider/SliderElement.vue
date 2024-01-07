958aee3cfb09771d16750627a6afece512ca3685
eldenhard <elden.hard@mail.ru>
2024-01-07 14:51:56 +0300

    main_page

--- src/modules/slider/SliderElement.vue
+++ src/modules/slider/SliderElement.vue
@@ -1,20 +1,9 @@
 <template>
   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" v-once>
     <div class="carousel-indicators">
-      <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="Slide 1"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide-to="1"
-        aria-label="Slide 2"
-      ></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="10000">
@@ -25,7 +14,7 @@
               <div class="rows"></div>
               <p class="appear_decription">
                 Стильные светопрозрачные конструкции, которые позволяют освободить максимально
-                широкий проем, соединить помещения между собой или с улицей. 
+                широкий проем, соединить помещения между собой или с улицей.
               </p>
               <br />
               <ButtonPurple :title="'Подробнее о разделе'">Узнать больше</ButtonPurple>
@@ -59,21 +48,11 @@
         </div>
       </div>
     </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExampleCaptions"
-      data-bs-slide="prev"
-    >
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExampleCaptions"
-      data-bs-slide="next"
-    >
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
@@ -118,9 +97,9 @@ export default {
 .left_text_carousel {
   /* background: rgb(1, 1, 2); */
   /* background: linear-gradient(177deg, rgba(1, 1, 2, 1) 7%, rgba(8, 44, 67, 1) 89%); */
-/* background: linear-gradient(177deg, rgba(1,1,2,1) 7%, rgba(4,24,37,1) 89%); */
-background: rgb(1,3,5);
-background: linear-gradient(177deg, rgba(1,3,5,1) 7%, rgba(4,21,33,1) 89%);
+  /* background: linear-gradient(177deg, rgba(1,1,2,1) 7%, rgba(4,24,37,1) 89%); */
+  background: rgb(1, 3, 5);
+  background: linear-gradient(177deg, rgba(1, 3, 5, 1) 7%, rgba(4, 21, 33, 1) 89%);
   height: 100%;
   z-index: 1;
   display: flex;
@@ -154,21 +133,43 @@ background: linear-gradient(177deg, rgba(1,3,5,1) 7%, rgba(4,21,33,1) 89%);
   left: 0;
   width: 100%;
   height: 30%;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7));
 }
 
 @media screen and (max-width: 900px) {
   .block_carousel {
     grid-template-columns: 1fr;
+    grid-template-rows: auto 0.4fr !important;
     padding-top: 6%;
+    margin-top: 10%;
+  }
+
+  .appear {
+    font-size: 18px;
+  }
+
+  .content {
+    width: 90%;
+  }
+
+  .rows {
+    display: none;
+  }
+
+  .appear_decription {
+    font-size: 12px;
   }
 
   .left_text_carousel {
     order: 1;
+    margin-bottom: 4%;
   }
 
   .right_text {
     order: 2;
+    max-height: 25vh;
+
   }
+
 }
 </style>
