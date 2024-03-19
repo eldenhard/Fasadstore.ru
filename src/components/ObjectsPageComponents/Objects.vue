bd169858b136697deb23251b6dc0258a483aa201
eldenhard <elden.hard@mail.ru>
2024-03-19 16:08:31 +0300

    адаптация под телефон граб курсор

--- src/components/ObjectsPageComponents/Objects.vue
+++ src/components/ObjectsPageComponents/Objects.vue
@@ -8,13 +8,13 @@
             </div>
             <div class="objects_body">
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/perevedenovskiy/6.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/perevedenovskiy/7.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/vatutinki/20140418_135437_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/vatutinki/S6000062_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/vatutinki/S6000064_11zon.webp" alt=""></swiper-slide>
@@ -22,26 +22,26 @@
                 </div>
            
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/dovil_old/20130301_123529.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/dovil_old/20130517_174130_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/dovil_old/20130517_180124-Photoroom_11zon.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/krasnye_vorota/DSC01198.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/krasnye_vorota/DSC01202.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/frazino/10_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/frazino/11_11zon.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/ugolnaya/20231010_115608_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/ugolnaya/20231010_115545_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/ugolnaya/IMG_0514_11zon.webp" alt=""></swiper-slide>
@@ -49,20 +49,20 @@
                 </div>
 
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/wolksvagen/VW-Varshavka1_b_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/wolksvagen/varshavskoe.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/wolksvagen/S6000462_11zon.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/volvo/yar_1b_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/volvo/123.webp" alt=""></swiper-slide>
                     </swiper>
                 </div>
                 <div class="objects_body_item">
-                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
                         <swiper-slide class="slide"><img src="./assets/2008_2013/novoostan/1_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/novoostan/S6002984_11zon.webp" alt=""></swiper-slide>
                         <swiper-slide class="slide"><img src="./assets/2008_2013/novoostan/S6002998_11zon.webp" alt=""></swiper-slide>
