21780b89ccb178745a8078b79acb1b1af319e3e3
eldenhard <elden.hard@mail.ru>
2024-03-21 11:59:18 +0300

    раздел 2013-2018

--- src/components/ObjectsPageComponents/Objects.vue
+++ src/components/ObjectsPageComponents/Objects.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="objects">
         <!--    <p class="gradient-text ">2018-2024</p>
-                <p class="gradient-text  right">2013-2018</p> -->
+                 -->
         <div class="objects_container">
             <div class="objects_title">
                 <p class="gradient-text">2008-2013</p>
@@ -69,6 +69,90 @@
                     </swiper>
                 </div>
             </div>
+            <p class="gradient-text  right">2013-2018</p>
+            <div class="objects_body">
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/kievka_47/IMG_1287_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/kievka_47/IMG_1289_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/winter_garden/20150522_130354_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/winter_garden/20150624_140005_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/winter_garden/20151010_134439_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/winter_garden/20151010_135056_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/gormoshka_iarat/20160206_154008_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/gormoshka_iarat/20160206_163931_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/gormoshka_iarat/20160215_120816_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/montevil/20160219_140813_11zon (1).webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/montevil/20160219_141042_11zon (1).webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/montevil/20160219_141214_11zon (1).webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/lumoni/20140418_134827_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/lumoni/20141008_150857_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/lumoni/20150218_135415_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/truvill/20141123_115049_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/truvill/IMG_1386_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/raisa/20130910_122012_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/raisa/20140512_173532_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/kashin/2_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/kashin/11zon_IMG_1160.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/zenitki/20140709_152121_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/zenitki/IMG_0329_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/zenitki/IMG_1298_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/new_selo/20140215_151434_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/new_selo/20140215_154533_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/new_selo/20140215_130859_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/solutions/3_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/solutions/4_11zon.webp" alt=""></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/solutions/var22_11zon.webp" alt=""></swiper-slide>
+                    </swiper>
+                </div>
+                <div class="objects_body_item">
+                    <swiper class="swiper" :modules="modules" navigation  :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :grab-cursor="true">
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/mini_cooper/IMG_0656_11zon.webp" alt="Остекление мини купер"></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/mini_cooper/IMG_1320_11zon.webp" alt="Остекление мини купер"></swiper-slide>
+                        <swiper-slide class="slide"><img src="./assets/2013_2018/mini_cooper/IMG_1330_11zon.webp" alt="Остекление мини купер"></swiper-slide>
+                    </swiper>
+                </div>
+            </div>
         </div>
 
 
@@ -90,6 +174,7 @@ export default {
         SwiperSlide
     },
     setup() {
+ 
         return {
             modules: [Navigation, Pagination]
         }
